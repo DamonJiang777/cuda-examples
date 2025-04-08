fc62bfbdc5cc05bfbc8100086814230d1c494174
jiangdazhi <jiangdz@tecorigin.com>
2025-04-08 13:53:14 +0800

    feat(format): add pre-commit and format current files

--- common/helper_timer.h
+++ common/helper_timer.h
@@ -2,192 +2,222 @@
 #define COMMON_HELPER_TIMER_H_
 
 // Definition of the StopWatch Interface, this is used if we don't want to use
-// the CUT functions But rather in a self contained class interface. 
-class StopWatchInterface {
-    public:
-        StopWatchInterface() {}
-        virtual ~StopWatchInterface() {}
-    public:
-        //! Start time measurement
-        virtual void start() = 0;
+// the CUT functions But rather in a self contained class interface.
+class StopWatchInterface
+{
+public:
+  StopWatchInterface() {}
+  virtual ~StopWatchInterface() {}
 
-        //! Stop time measurement
-        virtual void stop() = 0;
+public:
+  //! Start time measurement
+  virtual void start() = 0;
 
-        //! Reset time counters to zero
-        virtual void reset() = 0;
+  //! Stop time measurement
+  virtual void stop() = 0;
 
-        //! Time in msec. after start. If the stop watch is still running(i.e. there)
-        //! was no call to stop()) then the elaspsed time is returned, otherwise the
-        //! time between the last start() and stop call is returned
-        virtual float getTime() = 0;
+  //! Reset time counters to zero
+  virtual void reset() = 0;
 
-        //! Mean time to date based on the number of times the stopwatch has been
-        //! _stopped_ (ie finish sessions) and the current total time
-        virtual float getAverageTime() = 0;
+  //! Time in msec. after start. If the stop watch is still running(i.e. there)
+  //! was no call to stop()) then the elaspsed time is returned, otherwise the
+  //! time between the last start() and stop call is returned
+  virtual float getTime() = 0;
+
+  //! Mean time to date based on the number of times the stopwatch has been
+  //! _stopped_ (ie finish sessions) and the current total time
+  virtual float getAverageTime() = 0;
 };
 
 // Begin Stopwatch timer class definitions for all OS platforms
 #if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-    // only support linux
+// only support linux
 #else
 
 // declarations for Stopwatch on Linux and Mac OSX
-#include <sys/time.h>
-#include <ctime>
+#  include <ctime>
+#  include <sys/time.h>
 
-class StopWatchLinux : public StopWatchInterface {
-  public:
-    StopWatchLinux()
-        : start_time(),
-          diff_time(0.0),
-          total_time(0.0),
-          running(false),
-          clock_sessions(0) {}
+class StopWatchLinux : public StopWatchInterface
+{
+public:
+  StopWatchLinux()
+      : start_time()
+      , diff_time(0.0)
+      , total_time(0.0)
+      , running(false)
+      , clock_sessions(0)
+  {
+  }
 
-    virtual ~StopWatchLinux() {};
+  virtual ~StopWatchLinux() {};
 
-    //! start time measurement
-    inline void start();
+  //! start time measurement
+  inline void start();
 
-    //! stop time measurement
-    inline void stop();
+  //! stop time measurement
+  inline void stop();
 
-    //! reset time counters to zero
-    inline void reset();
+  //! reset time counters to zero
+  inline void reset();
 
-    //! Time in msec. after start. If the stop watch is still running(i.e. there)
-    //! was no call to stop()) then the elaspsed time is returned, otherwise the
-    //! time between the last start() and stop call is returned
-    inline float getTime();
+  //! Time in msec. after start. If the stop watch is still running(i.e. there)
+  //! was no call to stop()) then the elaspsed time is returned, otherwise the
+  //! time between the last start() and stop call is returned
+  inline float getTime();
 
-    //! Mean time to date based on the number of times the stopwatch has been
-    //! _stopped_ (ie finish sessions) and the current total time
-    inline float getAverageTime();
+  //! Mean time to date based on the number of times the stopwatch has been
+  //! _stopped_ (ie finish sessions) and the current total time
+  inline float getAverageTime();
 
-  private:
-    //! helper functions
+private:
+  //! helper functions
 
-    //! get difference between start time and current time
-    inline float getDiffTime();
+  //! get difference between start time and current time
+  inline float getDiffTime();
 
-  private:
-    //! start of measurement
-    struct timeval start_time;
+private:
+  //! start of measurement
+  struct timeval start_time;
 
-    //! time difference between the last start and stop
-    float diff_time;
+  //! time difference between the last start and stop
+  float diff_time;
 
-    //! Total time difference between stops and starts
-    float total_time;
+  //! Total time difference between stops and starts
+  float total_time;
 
-    //! flag if the stop watch is running
-    bool running;
+  //! flag if the stop watch is running
+  bool running;
 
-    //! number of times clock has been started
-    //！and stopped to allow averaging
-    int clock_sessions;
+  //! number of times clock has been started
+  // ！and stopped to allow averaging
+  int clock_sessions;
 };
 
-inline void StopWatchLinux::start() {
+inline void StopWatchLinux::start()
+{
+  gettimeofday(&start_time, 0);
+  running = true;
+}
+
+inline void StopWatchLinux::stop()
+{
+  diff_time = getDiffTime();
+  total_time += diff_time;
+  running = false;
+  clock_sessions++;
+}
+
+inline void StopWatchLinux::reset()
+{
+  diff_time      = 0;
+  total_time     = 0;
+  clock_sessions = 0;
+
+  if (running)
+  {
     gettimeofday(&start_time, 0);
-    running = true;
+  }
 }
 
-inline void StopWatchLinux::stop() {
-    diff_time = getDiffTime();
-    total_time += diff_time;
-    running = false;
-    clock_sessions++;
+inline float StopWatchLinux::getTime()
+{
+  float retval = total_time;
+  if (running)
+  {
+    retval += getDiffTime();
+  }
+  return retval;
 }
 
-inline void StopWatchLinux::reset() {
-    diff_time = 0;
-    total_time = 0;
-    clock_sessions = 0;
-
-    if (running) {
-        gettimeofday(&start_time, 0);
-    }
+inline float StopWatchLinux::getAverageTime()
+{
+  return (clock_sessions > 0) ? (total_time / clock_sessions) : 0.0f;
 }
 
-inline float StopWatchLinux::getTime() {
-    float retval = total_time;
-    if (running) {
-        retval += getDiffTime();
-    }
-    return retval;
-}
+inline float StopWatchLinux::getDiffTime()
+{
+  struct timeval t_time;
+  gettimeofday(&t_time, 0);
 
-inline float StopWatchLinux::getAverageTime() {
-    return (clock_sessions > 0) ? (total_time / clock_sessions) : 0.0f;
-}
-
-inline float StopWatchLinux::getDiffTime() {
-    struct timeval t_time;
-    gettimeofday(&t_time, 0);
-
-    return static_cast<float>(1000.0 * (t_time.tv_sec - start_time.tv_sec) + 
-        (0.001 * (t_time.tv_usec - start_time.tv_usec)));
+  return static_cast<float>(1000.0 * (t_time.tv_sec - start_time.tv_sec)
+                            + (0.001 * (t_time.tv_usec - start_time.tv_usec)));
 }
 #endif
 
-inline bool sdkCreateTimer(StopWatchInterface **timer_interface) {
+inline bool sdkCreateTimer(StopWatchInterface **timer_interface)
+{
 #if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-    // only support linux os
+  // only support linux os
 #else
-    *timer_interface = reinterpret_cast<StopWatchLinux*>(new StopWatchLinux());
+  *timer_interface = reinterpret_cast<StopWatchLinux *>(new StopWatchLinux());
 #endif
-    return (*timer_interface != NULL) ? true : false;
+  return (*timer_interface != NULL) ? true : false;
 }
 
-inline bool sdkDeleteTimer(StopWatchInterface **timer_interface) {
-    if (*timer_interface) {
-        delete *timer_interface;
-        *timer_interface = NULL;
-    }
+inline bool sdkDeleteTimer(StopWatchInterface **timer_interface)
+{
+  if (*timer_interface)
+  {
+    delete *timer_interface;
+    *timer_interface = NULL;
+  }
 
-    return true;
+  return true;
 }
 
-inline bool sdkStartTimer(StopWatchInterface** timer_interface) {
-    if (*timer_interface) {
-        (*timer_interface)->start();
-    }
-    
-    return true;
+inline bool sdkStartTimer(StopWatchInterface **timer_interface)
+{
+  if (*timer_interface)
+  {
+    (*timer_interface)->start();
+  }
+
+  return true;
 }
 
-inline bool sdkStopTimer(StopWatchInterface **timer_interface) {
-    if (*timer_interface) {
-        (*timer_interface)->stop();
-    }
+inline bool sdkStopTimer(StopWatchInterface **timer_interface)
+{
+  if (*timer_interface)
+  {
+    (*timer_interface)->stop();
+  }
 
-    return true;
+  return true;
 }
 
-inline bool sdkResetTimer(StopWatchInterface** timer_interface) {
-    if (*timer_interface) {
-        (*timer_interface)->reset();
-    }
+inline bool sdkResetTimer(StopWatchInterface **timer_interface)
+{
+  if (*timer_interface)
+  {
+    (*timer_interface)->reset();
+  }
 
-    return true;
+  return true;
 }
 
-inline float sdkGetAverageTimerValue(StopWatchInterface** timer_interface) {
-    if (*timer_interface) {
-        return (*timer_interface)->getAverageTime();
-    } else {
-        return 0.0f;
-    }
+inline float sdkGetAverageTimerValue(StopWatchInterface **timer_interface)
+{
+  if (*timer_interface)
+  {
+    return (*timer_interface)->getAverageTime();
+  }
+  else
+  {
+    return 0.0f;
+  }
 }
 
-inline float sdkGetTimerValue(StopWatchInterface** timer_interface) {
-    if (*timer_interface) {
-        return (*timer_interface)->getTime();
-    } else {
-        return 0.0f;
-    }
+inline float sdkGetTimerValue(StopWatchInterface **timer_interface)
+{
+  if (*timer_interface)
+  {
+    return (*timer_interface)->getTime();
+  }
+  else
+  {
+    return 0.0f;
+  }
 }
 
 #endif
--- example/asyncAPI/asyncAPI.cu
+++ example/asyncAPI/asyncAPI.cu
@@ -1,100 +1,106 @@
-#include <stdio.h>
-
-#include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
+#include <cuda_runtime.h>
+
+#include <stdio.h>
 
 #include "helper_cuda.h"
 #include "helper_timer.h"
 
-__global__ void increment_kernel(int* g_data, int inc_value) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    g_data[idx] += inc_value;
+__global__ void increment_kernel(int *g_data, int inc_value)
+{
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  g_data[idx] += inc_value;
 }
 
-bool correct_output(int *data, const int n, const int x) {
-    for (int i = 0; i < n; ++i) {
-        if (data[i] != x) {
-            printf("Error! data[%d] = %d, ref = %d\n", i, data[i], x);
-            return false;
-        }
+bool correct_output(int *data, const int n, const int x)
+{
+  for (int i = 0; i < n; ++i)
+  {
+    if (data[i] != x)
+    {
+      printf("Error! data[%d] = %d, ref = %d\n", i, data[i], x);
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-int main(int argc, char** argv) {
-    int device_id;
-    cudaDeviceProp device_props;
+int main(int argc, char **argv)
+{
+  int            device_id;
+  cudaDeviceProp device_props;
 
-    printf("[%s] - Starting...\n", argv[0]);
-    
-    device_id = FindCudaDevice(argc, (const char **)argv);
+  printf("[%s] - Starting...\n", argv[0]);
 
-    checkCudaErrors(cudaGetDeviceProperties(&device_props, device_id));
-    printf("CUDA device [%s]\n", device_props.name);
+  device_id = FindCudaDevice(argc, (const char **) argv);
 
-    int n = 16 * 1024 * 1024;
-    int nbytes = n * sizeof(int);
-    int value = 26;
+  checkCudaErrors(cudaGetDeviceProperties(&device_props, device_id));
+  printf("CUDA device [%s]\n", device_props.name);
 
-    // allocate host memory
-    int *a = 0;
-    checkCudaErrors(cudaMallocHost((void **)&a, nbytes));
-    memset(a, 0, nbytes);
+  int n      = 16 * 1024 * 1024;
+  int nbytes = n * sizeof(int);
+  int value  = 26;
 
-    // allocate device memory
-    int* d_a = 0;
-    checkCudaErrors(cudaMalloc((void **)&d_a, nbytes));
-    checkCudaErrors(cudaMemset(d_a, 255, nbytes));
+  // allocate host memory
+  int *a = 0;
+  checkCudaErrors(cudaMallocHost((void **) &a, nbytes));
+  memset(a, 0, nbytes);
 
-    // set kernel launch configuration
-    dim3 threads = dim3(512, 1);
-    dim3 blocks = dim3(n / threads.x, 1);
+  // allocate device memory
+  int *d_a = 0;
+  checkCudaErrors(cudaMalloc((void **) &d_a, nbytes));
+  checkCudaErrors(cudaMemset(d_a, 255, nbytes));
 
-    // create cuda event handles
-    cudaEvent_t start, stop;
-    checkCudaErrors(cudaEventCreate(&start));
-    checkCudaErrors(cudaEventCreate(&stop));
+  // set kernel launch configuration
+  dim3 threads = dim3(512, 1);
+  dim3 blocks  = dim3(n / threads.x, 1);
 
-    StopWatchInterface *timer = NULL;
-    sdkCreateTimer(&timer);
-    sdkResetTimer(&timer);
+  // create cuda event handles
+  cudaEvent_t start, stop;
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
 
-    checkCudaErrors(cudaDeviceSynchronize());
-    float gpu_time = 0.0f;
+  StopWatchInterface *timer = NULL;
+  sdkCreateTimer(&timer);
+  sdkResetTimer(&timer);
 
-    // asynchronously issue work to the GPU(all to stream 0)
-    checkCudaErrors(cudaProfilerStart());
-    sdkStartTimer(&timer);
-    cudaEventRecord(start, 0); // stream 0
-    cudaMemcpyAsync(d_a, a, nbytes, cudaMemcpyHostToDevice, 0); // stream 0
-    increment_kernel<<<blocks, threads, 0, 0>>>(d_a, value); // add value
-    cudaMemcpyAsync(a, d_a, nbytes, cudaMemcpyDeviceToHost, 0);
-    cudaEventRecord(stop, 0);
-    sdkStopTimer(&timer);
-    checkCudaErrors(cudaProfilerStop());
+  checkCudaErrors(cudaDeviceSynchronize());
+  float gpu_time = 0.0f;
 
-    // have CPU do some work while waiting for stage 1 to finish
-    unsigned long int counter = 0;
-    while (cudaEventQuery(stop) == cudaErrorNotReady) {
-        counter++;
-    }
+  // asynchronously issue work to the GPU(all to stream 0)
+  checkCudaErrors(cudaProfilerStart());
+  sdkStartTimer(&timer);
+  cudaEventRecord(start, 0);                                  // stream 0
+  cudaMemcpyAsync(d_a, a, nbytes, cudaMemcpyHostToDevice, 0); // stream 0
+  increment_kernel<<<blocks, threads, 0, 0>>>(d_a, value);    // add value
+  cudaMemcpyAsync(a, d_a, nbytes, cudaMemcpyDeviceToHost, 0);
+  cudaEventRecord(stop, 0);
+  sdkStopTimer(&timer);
+  checkCudaErrors(cudaProfilerStop());
 
-    checkCudaErrors(cudaEventElapsedTime(&gpu_time, start, stop));
+  // have CPU do some work while waiting for stage 1 to finish
+  unsigned long int counter = 0;
+  while (cudaEventQuery(stop) == cudaErrorNotReady)
+  {
+    counter++;
+  }
 
-    // print the cpu and gpu times
-    printf("time spent executing by the GPU: %.2f\n", gpu_time);
-    printf("time spent by CPU in CUDA calls: %.2f\n", sdkGetTimerValue(&timer));
-    printf("CPU executed %lu iterations while waitting for the GPU to finish\n", counter);
+  checkCudaErrors(cudaEventElapsedTime(&gpu_time, start, stop));
 
-    // check the output for correctness
-    bool b_final_results = correct_output(a, n, value);
+  // print the cpu and gpu times
+  printf("time spent executing by the GPU: %.2f\n", gpu_time);
+  printf("time spent by CPU in CUDA calls: %.2f\n", sdkGetTimerValue(&timer));
+  printf("CPU executed %lu iterations while waitting for the GPU to finish\n", counter);
 
-    // release resources
-    checkCudaErrors(cudaEventDestroy(start));
-    checkCudaErrors(cudaEventDestroy(stop));
-    checkCudaErrors(cudaFreeHost(a));
-    checkCudaErrors(cudaFree(d_a));
+  // check the output for correctness
+  bool b_final_results = correct_output(a, n, value);
 
-    exit(b_final_results ? EXIT_SUCCESS : EXIT_FAILURE);
-    return 0;
+  // release resources
+  checkCudaErrors(cudaEventDestroy(start));
+  checkCudaErrors(cudaEventDestroy(stop));
+  checkCudaErrors(cudaFreeHost(a));
+  checkCudaErrors(cudaFree(d_a));
+
+  exit(b_final_results ? EXIT_SUCCESS : EXIT_FAILURE);
+  return 0;
 }
